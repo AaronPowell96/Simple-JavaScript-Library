4b0c2adc3dd7c22f4015bad58217a082277302c0
Aaron Powell <Apowell829@gmail.com>
2018-12-20 18:20:40 +0000

    commenting

--- Starter/app.js
+++ Starter/app.js
@@ -1,5 +1,7 @@
+//gets new object without using new due to library
 var g = G$("John", "Doe");
 
+//greets in english and then in spanish, then formally, then english formally
 g.greet()
   .setLang("es")
   .greet()
@@ -7,10 +9,12 @@ g.greet()
   .setLang("en")
   .greet(true);
 
+//uses jQuery to get the login id, click event, calls the greet method with the name john doe.
 $("#login").click(function() {
   var loginGreetr = G$("John", "Doe");
 
   $("#loginDiv").hide();
 
+  //sets the language based on the value of the selector language.
   loginGreetr.setLang($("#lang").val()).HTMLGreeting("#greeting", true);
 });
