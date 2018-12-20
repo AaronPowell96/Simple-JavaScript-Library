12083499828cc70a74e85c26d839977e83489580
Aaron Powell <Apowell829@gmail.com>
2018-12-20 17:59:39 +0000

    Set html value with greeting using jQuery selector

--- Starter/Greetr.js
+++ Starter/Greetr.js
@@ -1,26 +1,32 @@
 //Safe area for reusable code in libraries etc IIFE
+//semicolon first justincase previous script didnt finish
 (function(global, $) {
+  //new object
   var Greetr = function(firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   };
-
-  var supportedLangs = ["en", "es"];
-
+  //hidden within scope of IIFE never directly accessible, list of supported languages
+  var supportedLangs = ["en", "es", "ar"];
+  //informal greetings
   var greetings = {
     en: "Hello",
-    es: "Hola"
+    es: "Hola",
+    ar: "Salaam"
   };
-
+  //formal greetings
   var formalGreetings = {
     en: "Greetings",
-    es: "Saludos"
+    es: "Saludos",
+    ar: "As-salāmu ʿalaykum"
   };
-
+  //message logs
   var logMessages = {
     en: "Logged in",
     es: "Inicio sesion"
   };
+  //sets methods of Greetr Prototype
   Greetr.prototype = {
+    //returns full name of person
     fullName: function() {
       return this.firstName + " " + this.lastName;
     },
@@ -70,6 +76,7 @@
       return this;
     },
 
+    //Uses jQuery selector to decide the language and print the greet
     HTMLGreeting: function(selector, formal) {
       if (!$) {
         throw "jQuery not loaded";
@@ -89,12 +96,15 @@
     }
   };
 
+  //the actual object created here, allowing us to new an object without calling new
   Greetr.init = function(firstName, lastName, language) {
     var self = this;
 
     self.firstName = firstName || "Enter First Name";
     self.lastName = lastName || "Enter Last Name";
     self.language = language || "en";
+
+    self.validate();
   };
   //Sets function of Greetr.init to have the same prototype as Greetr.
   Greetr.init.prototype = Greetr.prototype;
--- Starter/app.js
+++ Starter/app.js
@@ -6,3 +6,11 @@ g.greet()
   .greet(true)
   .setLang("en")
   .greet(true);
+
+$("#login").click(function() {
+  var loginGreetr = G$("John", "Doe");
+
+  $("#loginDiv").hide();
+
+  loginGreetr.setLang($("#lang").val()).HTMLGreeting("#greeting", true);
+});
